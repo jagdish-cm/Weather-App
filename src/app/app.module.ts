8a668df6e9d47273f9e69a1c25592b798aec0b3c
jagdish-cm <jagdishcm41297@gmail.com>
2020-10-20 02:03:45 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  apiKey = "51bee09a9fa071f14f3b9599c16f7537";
+  constructor(private http : HttpClient) { 
+    
+  }
+
+  getCityWeather(city : string) : Observable<any>{
+    let url = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey;
+    return this.http.get("http://" +url);
+  }
+}
