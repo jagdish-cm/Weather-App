4f83b6a46391c6255ce55a064319dc86b65693f3
jagdish-cm <jagdishcm41297@gmail.com>
2020-11-10 10:57:58 +0530

    a

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/WeatherApp'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/WeatherApp/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
